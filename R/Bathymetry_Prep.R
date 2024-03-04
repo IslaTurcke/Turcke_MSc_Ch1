893dc5ab460e938ddbb5a9b4b73dea43166efcfd
IslaTurcke <107211408+IslaTurcke@users.noreply.github.com>
2024-03-04 12:22:18 -0700

    Update Bathymetry_Prep.R
    Added section to take parks shapefiles, fill in gaps, find the union, then aggregate (dissolve) into one parks border shapefile.

--- R/Bathymetry_Prep.R
+++ R/Bathymetry_Prep.R
@@ -31,6 +31,7 @@ here::i_am("GitHub_Repositories/MSc_Ch1_DataPrep/R/Bathymetry_Prep.R")
 # DEFINE COORDINATE SYSTEMS 
 # project CRS - EPSG:6346 NAD 1983 (2011) UTM Zone 17N
 new_crs <- crs("+init=epsg:6346")
+old_crs <- crs("+init=epsg:26958")
 
 
 
@@ -126,20 +127,23 @@ rm(tile, i)
 cudem_sprc <- sprc(cudem_tiles)
 
 # merge the five tiles into one raster
-cudem_full <- merge(cudem_sprc)
+cudem_full_feet <- merge(cudem_sprc)
+
+# convert from feet to meters
+cudem_full_m <- cudem_full_feet * 0.3048
 
 # clean up storage
-rm(cudem_files, cudem_tiles, cudem_sprc)
+rm(cudem_files, cudem_tiles, cudem_sprc, cudem_full_feet)
 
 # project to the same coord system as the NOAA post-Irma DEM
-cudem_5x5 <- project(cudem_full, lidar_5x5, align = TRUE)
+cudem_5x5 <- project(cudem_full_m, lidar_5x5, align = TRUE)
 
 # Save the combined raster to a new file
 writeRaster(cudem_5x5, here("Intermediate_Data","cudem_full_5x5.tif"), 
             overwrite = TRUE, filetype = "GTiff")
 
 # clean up storage
-rm(cudem_full)
+rm(cudem_full_m)
 
 
 
@@ -149,10 +153,190 @@ rm(cudem_full)
 # combine the partly overlapping SpatRasters to form a single new SpatRaster
 # values in overlapping cells are averaged
 depth_5x5 <- mosaic(cudem_5x5, lidar_5x5)
-print(depth_5x5)
 
 # Save the combined raster to a new file
 writeRaster(depth_5x5, here("Intermediate_Data","depth_5x5.tif"), 
                             overwrite = TRUE, filetype = "GTiff")
 
+# print summary info
+print(cudem_5x5)
+print(lidar_5x5)
+print(depth_5x5)
 
+
+
+# Park Boundaries ---------------------------------------------------------
+
+
+# Read in park/marine sanctuary polygons
+# Florida Keys National Marine Sanctuary (FKNMS) shapefile 
+# (https://sanctuaries.noaa.gov/library/imast_gis.html)
+
+fknms_vect <- terra::vect(here("Source_Data","Parks","fknms_py.shp"))
+compareCRS(crs(fknms_vect), new_crs, verbatim = T, verbose = T) # check projection
+fknms_vect <- terra::project(fknms_vect, new_crs)
+compareCRS(crs(fknms_vect), new_crs, verbatim = T, verbose = T) # check projection again
+
+# National Park Service shapefile (for Biscayne National Park (BNP))
+# (https://public-nps.opendata.arcgis.com/datasets/nps-boundary-1/data)
+nps_vect <- terra::vect(here("Source_Data","Parks","NPS_-_Land_Resources_Division_Boundary_and_Tract_Data_Service.shp"))
+bnp_vect <- nps_vect[nps_vect$UNIT_NAME == "Biscayne National Park",] # extract BNP
+compareCRS(crs(bnp_vect), new_crs, verbatim = T, verbose = T)
+bnp_vect <- terra::project(bnp_vect, new_crs)
+compareCRS(crs(bnp_vect), new_crs, verbatim = T, verbose = T)
+
+# how do the shapefiles line up
+plot(fknms_vect, col = "turquoise")
+plot(bnp_vect, col = "purple", add = T)
+
+# there are gaps at the interior seams where FKNMS and BNP should meet, here is 
+# an arbitrary polygon that I constructed in a GIS to fill the interior gaps 
+# while still respecting the outer boundaries of the two parks
+fill_gaps <- data.frame(
+  lon = c(291433.147, 292034.632, 292232.734, 292246.008, 285348.704, 279047.242,
+          275554.735, 275369.526, 275054.142, 274774.742, 274393.741, 272105.355,
+          270054.830, 269509.126, 268907.462, 268944.004, 269174.427, 270765.899,
+          272195.446, 274481.451, 275137.619, 275518.619, 276132.454, 279709.628,
+          284056.211, 290049.036, 290684.037),
+  lat = c(145564.616, 146169.449, 146170.187, 132196.017, 103890.571, 110144.275, 
+          110223.650, 111678.862, 112384.771, 112841.972, 113121.373, 114417.569, 
+          114864.055, 115029.419, 114478.159, 114673.867, 115390.709, 115045.427,
+          114723.957, 113390.455, 113030.621, 112247.452, 111570.118, 111675.951, 
+          107164.796, 137644.857, 145185.497)) %>%
+  st_as_sf(coords = c("lon", "lat"), crs = old_crs) %>%
+  dplyr::summarise(geometry = st_combine(geometry)) %>%
+  st_cast("POLYGON")
+gaps_vect <- terra::vect(fill_gaps)
+compareCRS(crs(gaps_vect), crs(fknms_vect), verbatim = T, verbose = T)
+gaps_vect <- terra::project(gaps_vect, new_crs)
+compareCRS(crs(gaps_vect), crs(fknms_vect), verbatim = T, verbose = T)
+
+# take a look now
+plot(gaps_vect, col = "orange")
+plot(fknms_vect, col = "turquoise", add = T)
+plot(bnp_vect, col = "purple", add = T)
+
+# union the parks and gap fill data to produce one complete polygon
+parks_union <- terra::union(fknms_vect, bnp_vect)
+parks_union <- terra::union(parks_union, gaps_vect)
+parks_vect <- terra::aggregate(parks_union, by = NULL, dissolve = T)
+
+# we now have an outline now of the parks' outer borders! 
+plot(parks_union)
+plot(parks_vect)
+
+# output new parks shapefile to intermediate data folder
+writeVector(parks_vect, here("Intermediate_Data","parks_border.shp"), 
+            filetype = "ESRI Shapefile", overwrite = TRUE)
+
+# remove temporary data
+rm("fknms_vect", "bnp_vect", "nps_vect", "fill_gaps", "gaps_vect", "parks_union", "old_crs")
+
+
+
+# Habitat Type ------------------------------------------------------------
+
+
+# load habitat data from Unified Reef Tract Map (URM)
+# (https://myfwc.com/research/gis/regional-projects/unified-reef-map/)
+require(rgdal)
+fgdb <- paste0(source_wd, "Unified_Reef_Map/FWC_UnifiedFloridaReefMap_v2.0.gdb")
+
+# List all feature classes in the geodatabase
+subset(ogrDrivers(), grepl("GDB", name)) 
+fc_list = ogrListLayers(fgdb) 
+print(fc_list) 
+
+# Read in Unified Reef Map (URM) feature class
+reef_map <- st_read(dsn = fgdb,layer = "UnifiedReefMap") %>%
+  filter(!st_is_empty(.)) %>%
+  st_transform(., new_crs)
+compareCRS(crs(reef_map), new_crs) # check projection to be sure
+
+# check benthic habitat classes
+unique(reef_map$ClassLv1) 
+
+# create IDs because MaxEnt requires categorical variables to be defined 
+# numerically, not with words
+ClassLv1_list <- unique(reef_map$ClassLv1) # list categories
+# create a data.frame of IDs
+ClassLv1_df <- data.frame(ID = 1:length(ClassLv1_list), ClassLv1 = ClassLv1_list) 
+# match IDs
+reef_map$ClassLv1_ID <- ClassLv1_df$ID[match(reef_map$ClassLv1, ClassLv1_df$ClassLv1)] 
+unique(reef_map$ClassLv1_ID) # did it work?  
+# save for later
+write.csv(ClassLv1_df, paste0(gis_wd, "Habitat/URM_ClassLv1_IDs.csv"), row.names = F)
+
+# clip unified reef map data to parks shapefile
+reef_clip <- st_intersection(st_make_valid(reef_map), parks_union)
+
+# create a palette for plotting benthic habitat classes
+pal_benthic <- pnw_palette("Bay", 14, type = "continuous") 
+
+tm_shape(parks_union) +
+  tm_fill("pink") +
+  tm_shape(reef_clip, projection = new_crs) +
+  tm_fill("ClassLv1_ID", palette = pal_benthic, style = "cat")
+
+
+# supplementary shoreline mangrove habitat data
+# (https://geodata.myfwc.com/datasets/mangrove-habitat-in-florida-1/explore)
+mg_shore <- st_read(dsn = paste0(source_wd, 
+                                 "Mangrove_Habitat/Mangrove_Habitat_in_Florida.shp")) %>%
+  filter(!st_is_empty(.)) %>%
+  st_transform(., new_crs) %>%
+  st_cast(., "MULTIPOLYGON")
+compareCRS(crs(mg_shore), new_crs)
+
+# there is a small gap between the mangrove data and the reef tract map along 
+# the mainland coast, and these missing areas are important coastline mangroves 
+# (visible in satellite imagery) add a 10 m buffer around the mangrove data and 
+# then use gDifference to keep only the non-overlapping areas (AKA, respect the 
+# boundaries of the reef map).
+mg_buff <- st_buffer(mg_shore, dist = 10)
+
+# now keep only the non-overlapping regions of the mangrove data 
+# (FYI: time-consuming step)
+mg_clip <- rgeos::gDifference(as_Spatial(st_intersection(mg_buff, parks_union)),
+                              as_Spatial(reef_clip)) %>% st_as_sf()
+
+# double check the ID assigned to mangroves from the reef map and add it to the
+# mangrove data
+ClassLv1_df
+mg_clip$ClassLv1 <- rep(as.character("Mangrove"), nrow(mg_clip))
+mg_clip$ClassLv1_ID <- rep(as.integer(11), nrow(mg_clip))
+
+# save mangrove and reef data for mapping
+st_write(mg_clip, dsn = paste0(gis_wd, "Habitat/Mangrove_Habitat.shp"), 
+         driver = "ESRI Shapefile", append = F)
+st_write(reef_clip, dsn = paste0(gis_wd, "Habitat/Reef_Map_Habitat.shp"), 
+         driver = "ESRI Shapefile", append = F)
+
+#  check geometry for both habitat layers
+head(mg_clip, 1) # sf column: geometry 
+head(reef_clip, 1) # sf column: Shape
+reef_clip = reef_clip %>% rename(geometry = Shape)
+head(reef_clip, 1) # fixed
+
+# combining the reef and mangrove habitat layers
+# filtering out classes "Land" and "Not Classified"
+habitat_polys <- rbind(mg_clip, select(reef_clip, geometry, ClassLv1, ClassLv1_ID)) %>%
+  filter(!ClassLv1 %in% c("Land", "Not Classified")) %>% # only want real benthic habitats
+  st_cast("MULTIPOLYGON")
+
+# plot habitat type polygons
+tm_shape(habitat_polys, projection = new_crs) +
+  tm_fill("ClassLv1_ID", palette = pal_benthic, style = "cat") 
+
+# write out habitat type polygons
+st_write(habitat_polys, dsn = paste0(gis_wd, "Habitat/Habitat_Type.shp"), append = F)
+
+# template raster for habitat w/ 1 x 1 m res and standard CRS
+template_raster <- raster(ext = extent(habitat_polys), res = c(1,1), crs = new_crs)
+
+# convert benthic habitat polygons to a temporary raster
+habitat_raster <- fasterize(habitat_polys, template_raster, field = "ClassLv1_ID", fun = "max") 
+
+# write out habitat type raster to temporary folder
+# once it is clipped to the final study domain it will be the final habitat raster
+writeRaster(habitat_raster, file = file.path(gis_wd, "Habitat/habitat_raster.tif"), format = "GTiff", overwrite = T)
